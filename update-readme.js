86ea2e219673f3e5f390fd1bb93cf5b768640204
Anthony Robert <99475715+AnthonyRobert377@users.noreply.github.com>
2025-05-01 14:57:16 +0100

    Update update-readme.js

--- update-readme.js
+++ update-readme.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 
+// Calculate years since July 2016
 const startDate = new Date('2016-07-01');
 const now = new Date();
 
@@ -11,19 +12,54 @@ if (
   experience--;
 }
 
-const readmePath = './README.md';
-let readme = fs.readFileSync(readmePath, 'utf-8');
+// Full README content
+const readme = `
+<h1>Hey! I'm Anthony.</h1>
+<p>Web Developer with ${experience} years experience!</p>
 
-// Create a temp version with the actual year for pushing
-const output = readme.replace(
-  '[EXPERIENCE_YEARS]',
-  `${experience}`
-);
+<h2>Tech Stack</h2>
+<h3>Software</h3>
 
-// Push that version
-fs.writeFileSync(readmePath, output);
+![Adobe After Effects](https://img.shields.io/badge/Adobe%20After%20Effects-9999FF.svg?style=for-the-badge&logo=Adobe%20After%20Effects&logoColor=white)
+![Adobe Dreamweaver](https://img.shields.io/badge/Adobe%20Dreamweaver-FF61F6.svg?style=for-the-badge&logo=Adobe%20Dreamweaver&logoColor=white)
+![Adobe Illustrator](https://img.shields.io/badge/adobe%20illustrator-%23FF9A00.svg?style=for-the-badge&logo=adobe%20illustrator&logoColor=white)
+![Adobe Photoshop](https://img.shields.io/badge/adobe%20photoshop-%2331A8FF.svg?style=for-the-badge&logo=adobe%20photoshop&logoColor=white)
+![Adobe Premiere Pro](https://img.shields.io/badge/Adobe%20Premiere%20Pro-9999FF.svg?style=for-the-badge&logo=Adobe%20Premiere%20Pro&logoColor=white)
+![ChatGPT](https://img.shields.io/badge/chatGPT-74aa9c?style=for-the-badge&logo=openai&logoColor=white)
+![Cloudflare](https://img.shields.io/badge/Cloudflare-F38020?style=for-the-badge&logo=Cloudflare&logoColor=white)
+![Figma](https://img.shields.io/badge/figma-%23F24E1E.svg?style=for-the-badge&logo=figma&logoColor=white)
+![Git](https://img.shields.io/badge/git-%23F05033.svg?style=for-the-badge&logo=git&logoColor=white)
+![GitHub](https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white)
+![Microsoft](https://img.shields.io/badge/Microsoft-0078D4?style=for-the-badge&logo=microsoft&logoColor=white)
+![PhpStorm](https://img.shields.io/badge/phpstorm-143?style=for-the-badge&logo=phpstorm&logoColor=black&color=black&labelColor=darkorchid)
+![Rss](https://img.shields.io/badge/rss-F88900?style=for-the-badge&logo=rss&logoColor=white)
+![Sublime Text](https://img.shields.io/badge/sublime_text-%23575757.svg?style=for-the-badge&logo=sublime-text&logoColor=important)
+![Visual Studio](https://img.shields.io/badge/Visual%20Studio-5C2D91.svg?style=for-the-badge&logo=visual-studio&logoColor=white)
+![Wix](https://img.shields.io/badge/wix-000?style=for-the-badge&logo=wix&logoColor=white)
+![WordPress](https://img.shields.io/badge/WordPress-%23117AC9.svg?style=for-the-badge&logo=WordPress&logoColor=white)
 
-// Then restore the placeholder version for next run (optional)
-setTimeout(() => {
-  fs.writeFileSync(readmePath, readme);
-}, 2000);
+<h3>Languages</h3>
+
+![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)
+![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+![MySQL](https://img.shields.io/badge/mysql-%2300f.svg?style=for-the-badge&logo=mysql&logoColor=white)
+![PHP](https://img.shields.io/badge/php-%23777BB4.svg?style=for-the-badge&logo=php&logoColor=white)
+![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54)
+![SASS](https://img.shields.io/badge/SASS-hotpink.svg?style=for-the-badge&logo=SASS&logoColor=white)
+
+<h2>GitHub Stats</h2>
+
+![](https://github-readme-streak-stats.herokuapp.com/?user=AnthonyRobert377&theme=tokyonight&hide_border=true)<br/>
+![](https://github-readme-stats.vercel.app/api/top-langs/?username=AnthonyRobert377&theme=tokyonight&hide_border=true&include_all_commits=true&count_private=true&layout=compact)
+
+<h3>:trophy: Trophies</h3>
+
+![](https://github-profile-trophy.vercel.app/?username=AnthonyRobert377&theme=tokyonight&no-frame=true&no-bg=true&margin-w=4)
+
+<h3>:two_men_holding_hands: Visitors</h3>
+
+![Visitor Count](https://profile-counter.glitch.me/AnthonyRobert377/count.svg)
+`;
+
+fs.writeFileSync('README.md', readme);
